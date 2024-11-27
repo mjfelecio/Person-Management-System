8a2c6a517203db23ddfc3879d54dfc308d311f02
mjfelecio <147979567+mjfelecio@users.noreply.github.com>
2024-11-27 10:46:39 +0800

    Roll back previous changes

--- js/utilities.js
+++ js/utilities.js
@@ -1,24 +1,27 @@
 let isOtherGenderSelected;
 let isOtherGenderSelectedOnModal;
 
-export function getFormInputs(isModal = false) {
-    if (!isModal) {
-        return {
-            fullName: document.getElementById("fullName").value.trim(),
-            gender: document.getElementById("gender").value === "Other"
-                ? document.getElementById("otherGender").value.trim()
-                : document.getElementById("gender").value.trim(),
-            birthDay: document.getElementById("birthDay").value.trim(),
-            occupation: document.getElementById("occupation").value.trim(),
-        };
-    } else {
-        return {
-            fullName: document.querySelector("#modalBody #fullName").value,
-            gender: document.querySelector("#modalBody #gender").value === "Other"
-                ? document.querySelector("#modalBody #otherGender").value
-                : document.querySelector("#modalBody #gender").value,
-            birthDay: document.querySelector("#modalBody #birthDay").value,
-            occupation: document.querySelector("#modalBody #occupation").value,
-        };
-    }
+// ==================================
+//  Form Utilities
+// ==================================
+export function getFormInputs() {
+    return {
+        fullName: document.getElementById("fullName").value.trim(),
+        gender: document.getElementById("gender").value === "Other"
+            ? document.getElementById("otherGender").value.trim()
+            : document.getElementById("gender").value.trim(),
+        birthDay: document.getElementById("birthDay").value.trim(),
+        occupation: document.getElementById("occupation").value.trim(),
+    };
+}
+
+export function getUpdatedData() {
+    return {
+        fullName: document.querySelector("#modalBody #fullName").value,
+        gender: document.querySelector("#modalBody #gender").value === "Other"
+            ? document.querySelector("#modalBody #otherGender").value
+            : document.querySelector("#modalBody #gender").value,
+        birthDay: document.querySelector("#modalBody #birthDay").value,
+        occupation: document.querySelector("#modalBody #occupation").value,
+    };
 }
